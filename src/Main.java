d10aac4c239d4edd9dbcaabe411702cd5584fcce
Justin Nelson <justin.john.nelson@gmail.com>
2014-02-23 17:42:54 -0600

    Second half of repackage

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import java.sql.DriverManager;
 import java.util.List;
 import java.util.Scanner;
 
+import jdapper.JDapper;
+
 public class Main {
 	public static void main(String[] args) throws Exception {
 		String url = "jdbc:mysql://localhost:3307/";
