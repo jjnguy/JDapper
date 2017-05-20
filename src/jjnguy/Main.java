e8eb4780d1e17c629d52b3fd3a738464f5c65de5
Justin Nelson <justin.john.nelson@gmail.com>
2017-05-19 20:55:48 -0500

    format Main

--- src/jjnguy/Main.java
+++ src/jjnguy/Main.java
@@ -2,24 +2,27 @@ package jjnguy;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.List;
 
 import jjnguy.jdapper.JDapper;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		String url = "jdbc:mysql://localhost:3307/";
-		String dbName = "test"; //"world";
-		String driver = "com.mysql.jdbc.Driver";
-		String userName = "jjnguy";
-		String password = "asdf1234";
-		Class.forName(driver).newInstance();
-		Connection conn = DriverManager.getConnection(url + dbName, userName, password);
-		JDapper jd = new JDapper(conn);
+    public static void main(String[] args) throws Exception {
+        String url = "jdbc:mysql://localhost:3307/";
+        String dbName = "test"; //"world";
+        String driver = "com.mysql.jdbc.Driver";
+        String userName = "jjnguy";
+        String password = "asdf1234";
+        Class.forName(driver).newInstance();
+        Connection conn = DriverManager.getConnection(url + dbName, userName, password);
+        JDapper jd = new JDapper(conn);
 
-		Stuff s = new Stuff();
-		s.string1 = "Hi youtube";
-		s.number1 = 42;
-		
-		jd.insert("stuff", s);
-	}
+        Stuff s = new Stuff();
+        s.string1 = "Hi youtube";
+        s.number1 = 42;
+
+        List<Country> countries = jd.query("SELECT * FROM [Table]", Country.class);
+
+        jd.insert("stuff", s);
+    }
 }
